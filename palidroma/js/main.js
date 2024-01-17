00c0f38486e8d2f04779f542fb3cfa0468abf5f2
GiPisano <pisanogiacomo9@gmail.com>
2024-01-17 19:03:51 +0100

    add user input control for palidroma file

--- palidroma/js/main.js
+++ palidroma/js/main.js
@@ -1,3 +1,6 @@
 // chiedo all'utente di inserire una parola
 let userWord = prompt('inserisci una paraola');
+while(!isNaN(userWord) || userWord.includes(' ')){
+    userWord = prompt('i dati inseriti non sono corretti, inserisci una sola parola');
+}
 palindromicWord(userWord);
